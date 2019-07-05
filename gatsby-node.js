95a1ac727b2be844ce9dc361cc88992cb89ea219
Ben Cox <ben.cox@uk.ibm.com>
2019-07-05 12:17:16 +0100

    Rename template to docTemplate

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,7 +3,7 @@ const path = require("path")
 exports.createPages = ({ actions, graphql }) => {
   const { createPage } = actions
 
-  const blogPostTemplate = path.resolve(`src/templates/pageTemplate.js`)
+  const docTemplate = path.resolve(`src/templates/docTemplate.js`)
 
   return graphql(`
     {
@@ -27,7 +27,7 @@ exports.createPages = ({ actions, graphql }) => {
     result.data.allMarkdownRemark.edges.forEach(({ node }) => {
       createPage({
         path: node.frontmatter.path,
-        component: blogPostTemplate,
+        component: docTemplate,
         context: {}, // additional data can be passed via context
       })
     })
