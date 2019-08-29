2b79deda9b2d7735e25a769d57f8771766e69f02
Caroline S McNamara <caroline.mcnamara@uk.ibm.com>
2019-08-29 14:00:33 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,9 +11,8 @@ const IndexPage = () => (
     <div className={indexStyles.heroContainer} />
     <div className={indexStyles.keyFeatureContainer}>
       <KeyFeature title="Lighten the load with automation">
-        Let Galasa take care of repetitive, time-consuming manual regression
-        tests, so you can focus on designing more complex test cases to root out
-        critical application defects.
+        Run reliable, repeatable, scalable, automated Galasa integration tests instead of time-consuming manual ones. 
+        Use the time saved to focus on designing more complex tests to root out those hard-to-find application defects.
       </KeyFeature>
       <KeyFeature title="Integrate across systems">
         Galasa integrates easily into your{" "}
@@ -31,21 +30,17 @@ const IndexPage = () => (
       </KeyFeature>
       <KeyFeature title="Fly through test cases">
         Run large numbers of tests across different environments in parallel by
-        using automation. Test output is automatically recorded and stored in a
-        single repository, making it easier to quickly identify the cause of a
-        failure.
+        using automation. 
       </KeyFeature>
-      <KeyFeature title="Deliver with confidence">
-        Run reliable, repeatable, automated test suites to deliver changes
-        quickly and with confidence. Dynamically build a test catalog from your
-        test code and automatically select relevant regression test cases for a
-        specific changeset.
+      <KeyFeature title="Speed up diagnosis">
+        Test output is automatically recorded and stored in a single repository,
+        making it easier to quickly identify the cause of failure. 
       </KeyFeature>
       <KeyFeature title="Environmentally friendly">
         Run the same test in different test environments without changing a
         single line of code. Speed up problem determination by running test code
         locally on your laptop in debug mode or run a test suite overnight using
-        automation. Use an existing z/OS environment, or get an environment
+        automation. Use an existing mainframe environment, or get an environment
         provisioned specifically for you.
       </KeyFeature>
     </div>
