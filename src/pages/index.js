7130459b33532886f2d3aba34d28adf97afccbf2
Caroline S McNamara <caroline.mcnamara@uk.ibm.com>
2019-09-25 13:51:43 +0100

    Update L. page and rem dev pipeline from toc
    update l page and toc

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ const IndexPage = () => (
           Deep integration testing for power&#8209;seeking testers
         </h1>
         <p className={indexStyles.heroDescription}>
-          Start running repeatable, reliable automated tests and deliver your
+          Start running repeatable, reliable, automated tests and deliver your
           software changes faster and with confidence.
         </p>
         <div className={indexStyles.heroButtons}>
@@ -34,7 +34,7 @@ const IndexPage = () => (
       </KeyFeature>
       <KeyFeature title="Integrate across systems">
         Galasa integrates easily into your{" "}
-        <Link to="/about/devops">DevOps pipeline</Link> and can be used
+        <Link to="/about/devops">DevOps strategy</Link> and can be used
         alongside other test tools, such as JMeter and WSIM. Galasa can access
         z/OS files and commands, and comes with built-in support for integration
         with z/OS subsystems.
@@ -53,14 +53,14 @@ const IndexPage = () => (
       </KeyFeature>
       <KeyFeature title="Speed up diagnosis">
         Galasa automatically records test artifacts and stores the results in a
-        single repository, making it easier to quickly identify the cause of
+        single repository, making it easier to quickly identify the cause of a
         failure.
       </KeyFeature>
       <KeyFeature title="Environmentally friendly">
         With Galasa you can run the same test in different test environments
         without changing a single line of code. Speed up problem determination
         by running test code locally on your laptop in debug mode or run a test
-        suite overnight using automation.
+        suite overnight by using automation.
       </KeyFeature>
     </div>
     <Footer />
