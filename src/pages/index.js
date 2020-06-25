b770166f4af9d950f55088d3dfa456daddb3e7f4
Caroline S McNamara <caroline.mcnamara@uk.ibm.com>
2020-06-25 10:12:44 +0100

    update landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,70 +9,74 @@ import Button from "../components/button"
 
 const keyFeatures = [
   {
-    title: "Lighten the load with automation",
+    title: "Consistent testing - for all technologies",
     body: (
       <>
-        With Galasa you can run reliable, repeatable, scalable, automated
-        integration tests instead of time-consuming manual ones. Use the time
-        you save to focus on designing more complex tests to root out those
-        hard-to-find application defects.
+        You can write Galasa tests as a JUnit-style Java class or using the
+        Gherkin syntax and run them locally from your IDE or in automation
+        without having to change a single line of code. Deep z/OS integration
+        means you can verify data by interrogating the CICS application directly
+        and gives you the ability to check other z/OS resources, such as
+        messages on queues, without using stubs or mocking.
       </>
     ),
   },
   {
-    title: "Integrate across systems",
+    title: "Focus on the test – not the integration problems",
     body: (
       <>
-        Whether your application runs in z/OS, on Linux, inside a container, or
-        as combination of those, Galasa can interrogate all parts of that
-        application and integrate a mix of tooling into a single test. When
-        you’re ready, Galasa integrates easily into your{" "}
-        <Link to="/about/devops">DevOps strategy</Link>, and can be used
-        alongside other test tools.
+        Galasa’s framework enables deep integration tests to be run across
+        multiple technologies. One test case can interact with 3270, Selenium,
+        JMeter, batch jobs and more. When you’re ready, Galasa integrates easily
+        into your <Link to="/about/devops">DevOps strategy</Link>, and can be
+        used alongside other test tools.
       </>
     ),
   },
   {
-    title: "Get the test data you need, fast",
+    title: "Get the test data you need - fast",
     body: (
       <>
         Galasa can integrate with your test data strategy by helping you to
         provision new or existing test data records. Galasa can also lock test
         data so that many tests can run in parallel against the same test data
         pool, making it easy to repeat tests under identical conditions for
-        swift debugging.
+        swift debugging. Provisioned environments are automatically
+        deprovisioned at test completion, leaving a clean test system state
+        ready for the next tests.
       </>
     ),
   },
   {
-    title: "Fly through test cases",
+    title: "See the big picture – from one location",
     body: (
       <>
-        Use Galasa to run large numbers of tests across different environments
-        in parallel - no environment is too complex. You can use an existing
-        environment, or provision an environment specifically for your own use.
+        Test results and artifacts are stored in a single location in a uniform
+        style, enabling easy extraction of big picture information. A single
+        place to search all test output makes it easy to quickly identify the
+        cause of a failure and run tests locally to assist with debugging.
       </>
     ),
   },
   {
-    title: "Speed up diagnosis",
+    title: "Test planning and recording – made easy",
     body: (
       <>
-        Galasa automatically records test artifacts and stores the results in a
-        single repository - a single place to search all test output. This makes
-        it easy to quickly identify the cause of a failure and run tests locally
-        to assist with debugging.
+        The ability to create a test catalog within Galasa means that you can
+        easily define areas under test, enabling automation of even the most
+        arduous manual tests and making it easy to record and plan what tests
+        have run and what is left to do.
       </>
     ),
   },
   {
-    title: "Environmentally friendly",
+    title: "Keep on growing – Galasa grows with you",
     body: (
       <>
-        With Galasa you can run the same test in different test environments
-        without changing a single line of code. Provisioned environments are
-        automatically deprovisioned at test completion, leaving a clean test
-        system state ready for the next tests.
+        Galasa is open source, so can be extended to support additional tooling
+        with no vendor lock-in and no initial cost. The framework supports
+        enterprise level through put, as the test workload can be scaled
+        horizontally in its own cloud environment.
       </>
     ),
   },
