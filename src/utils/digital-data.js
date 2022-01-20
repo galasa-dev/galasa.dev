9e5896ba14788bbe606cf84ba45669dc53694d81
Ben Cox <1038350+ind1go@users.noreply.github.com>
2022-01-20 20:47:14 +0000

    Prettier

--- src/utils/digital-data.js
+++ src/utils/digital-data.js
@@ -1,3 +1,3 @@
 const digitalDataScript = `window.idaPageIsSPA=true;window.digitalData={page:{category:{primaryCategory:'NO_CATEGORY_ASSIGNED'},pageInfo:{ibm:{siteID:'IBM_NONMARKETING'}}}}`
 
-module.exports = digitalDataScript;
+module.exports = digitalDataScript
