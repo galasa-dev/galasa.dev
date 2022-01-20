8f1d2ca28e38cc4287be89b947d9e8e614a93e95
Ben Cox <1038350+ind1go@users.noreply.github.com>
2022-01-20 20:44:37 +0000

    Add Content-Security-Policy meta

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,7 @@
 const React = require("react")
 
+const digitalData = require("./src/utils/digital-data")
+
 if (process.env.GATSBY_GALASA_ENV !== "LOCAL") {
   console.log("Non-local build - adding tracking.")
 
@@ -7,23 +9,9 @@ if (process.env.GATSBY_GALASA_ENV !== "LOCAL") {
     
     const totalComponents = getHeadComponents()
     totalComponents.push(<script type="text/javascript" dangerouslySetInnerHTML={{
-      __html: `
-        window.idaPageIsSPA = true;
-        window.digitalData = {
-          page: {
-            category: {
-              primaryCategory: 'NO_CATEGORY_ASSIGNED',
-            },
-            pageInfo: {
-              ibm: {
-                  siteID: 'IBM_NONMARKETING',
-              }
-            },
-          }
-        };
-      `
+      __html: digitalData
     }}></script>)
-    totalComponents.push(<script src="//1.www.s81c.com/common/stats/ibm-common.js" type="text/javascript" async></script>)
+    totalComponents.push(<script src="https://1.www.s81c.com/common/stats/ibm-common.js" type="text/javascript" async></script>)
     replaceHeadComponents(totalComponents)
   }
 } else {
---
+++ src/utils/digital-data.js
@@ -0,0 +1,3 @@
+const digitalDataScript = `window.idaPageIsSPA=true;window.digitalData={page:{category:{primaryCategory:'NO_CATEGORY_ASSIGNED'},pageInfo:{ibm:{siteID:'IBM_NONMARKETING'}}}}`
+
+module.exports = digitalDataScript;
